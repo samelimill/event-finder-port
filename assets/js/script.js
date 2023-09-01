e5c52838b491faf9b0d9874f13e6fe3f7bd950e0
samelimill <samelimill@gmail.com>
2023-08-31 21:43:26 -0700

    added weather fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-
-
 function getEvents() {
     var ticketMaster = 'https://app.ticketmaster.com/discovery/v2/events.json?apikey=hMHxReixSyCV55s9yGYRjwi8uBBo39wM';
     fetch(ticketMaster)
@@ -12,23 +10,16 @@ function getEvents() {
     });
 }
 
-function getHotels() {
-    var hotelSearch = 'https://hotels4.p.rapidapi.com/locations/v3/search?q=new%20york&locale=en_US&langid=1033&siteid=300000001';
-    fetch(hotelSearch, {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '8aa5434ef1mshb3c71b6f7580706p15e80fjsn32f11792263e',
-            'X-RapidAPI-Host': 'hotels4.p.rapidapi.com'
-        }
+var weatherAPIKey = '1306dd10117d4dc1aff35143230109';
+var searchVal = 'Portland, OR';
+fetch ('https://api.weatherapi.com/v1/forecast.json?key='+weatherAPIKey+'&q='+searchVal+'&days=0')
+    .then(function (response) {
+        return response.json();
     })
-    .then(function(response) {
-        if(response.ok) {
-            response.json().then(function(data){
-                console.log(data)
-            })
-        }
+    .then(function (data) {
+        console.log(data)
+        var sunset = data.forecast.forcastDay[0].astro.sunset;
+        var eveningTemp = data.forecast.forecastDay[0].hour[21].temp_f;
+        var eveningCondition = data.forecast.forecastDay[0].hour[21].condition.text;
+        var eveningRainChance = data.forecast.forecastDay[0].hour[21].chance_of_rain;
     });
-}
-
-
-
