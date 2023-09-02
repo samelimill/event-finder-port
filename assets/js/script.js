ba83974d6dcce25e8472a1ca61b1c2519ba75777
hewman82 <138827425+hewman82@users.noreply.github.com>
2023-09-01 18:57:04 -0700

    display events list with title, date, and time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,53 @@
-function getEvents() {
-    var ticketMaster = 'https://app.ticketmaster.com/discovery/v2/events.json?apikey=hMHxReixSyCV55s9yGYRjwi8uBBo39wM';
+var searchBar = document.querySelector('.uk-search-field');
+var submitBtn = document.querySelector('#submit-button');
+var eventCon = document.querySelector('#event-container');
+var eventsList = document.createElement('ul');
+eventCon.appendChild(eventsList);
+
+
+submitBtn.addEventListener('click', function getInput(e) {
+    e.preventDefault;
+    var search = searchBar.value.toLowerCase();
+    var state = search.substr(search.length-3, 3);
+    var city = search.substr(0, search.length-4);
+    getEvents(city, state);
+    //getWeather(search); //wrap weather fetch in a function?
+})
+
+
+function getEvents(city, state) {
+    var ticketMaster = 'https://app.ticketmaster.com/discovery/v2/events.json?city=' + city + '&stateCode=' + state + '&classificationName=music&apikey=hMHxReixSyCV55s9yGYRjwi8uBBo39wM';
     fetch(ticketMaster)
     .then(function(response) {
         if(response.ok) {
             response.json().then(function(data){
-                console.log(data)
+                console.log(data);
+                displayEvents(data);
             })
         }
     });
 }
+
+function displayEvents(data) {
+    for(i = 0; i < data._embedded.events.length; i++) {
+        eventEl = document.createElement('li');
+        var date = data._embedded.events[i].dates.start.localDate;
+        var localTime = data._embedded.events[i].dates.start.localTime;
+        var hour = localTime.substr(0, 2);
+        var minutes = localTime.substr(3, 2);
+        var AmOrPm = hour >= 12 ? 'pm' : 'am'; //via medium.com, how to convert 24 hours format to 12 hours
+        hour = (hour % 12) || 12; //via medium.com
+        var finalTime = hour + ':' + minutes + AmOrPm;
+        
+        eventEl.textContent = data._embedded.events[i].name + ' | ' + date + ' ' + finalTime;
+        eventCon.appendChild(eventEl);
+    }
+}
+
+eventCon.addEventListener('click', function furtherDetails(e) {
+    var expanded = e.target;
+
+})
 
 var weatherAPIKey = '1306dd10117d4dc1aff35143230109';
 var searchVal = 'Portland, OR';
